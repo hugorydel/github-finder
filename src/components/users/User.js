fadfa688b885375eac2a8a548581930bfef1116b
Hugo Rydel <hugosteczkowski@gmail.com>
2020-03-29 15:40:58 -0400

    Single User Components & Data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -3,8 +3,33 @@
 import React, { Component } from 'react';
 
 export class User extends Component {
+  componentDidMount() {
+    const { getUser, match } = this.props;
+    //Here we're calling the Class's props which now contains getUser as in App.js we passed it into here.
+    //React Router will supply any dynamic pieces of the URL to the component via an object called match.params as own props of the related component which means that we can access the login
+    //The param of login is the path that we've set in App.js = "/user/:login"
+    getUser(match.params.login);
+  }
   render() {
-    return <div>User</div>;
+    //Pulls all the data gathered from the user
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    return <div>{name}</div>;
   }
 }
 
