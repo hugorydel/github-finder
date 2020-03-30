99a1b61523ea295ba6f5389bd73b14607b7fa4bd
Hugo Rydel <hugosteczkowski@gmail.com>
2020-03-30 19:03:09 -0400

    Context 6

--- src/context/alert/alertReducer.js
+++ src/context/alert/alertReducer.js
@@ -1,6 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 export default (state, action) => {
+  console.log(`Dispatched: ${action.type} Payload:`, action.payload);
   switch (action.type) {
     case SET_ALERT:
       //Returns the message and its type
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,46 +1,47 @@
 import {
-	SEARCH_USERS,
-	GET_USER,
-	CLEAR_USERS,
-	GET_REPOS,
-	SET_LOADING,
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USERS,
+  GET_REPOS,
+  SET_LOADING,
 } from '../types';
 
 //reducer
 export default (state, action) => {
-	switch (action.type) {
-		case SEARCH_USERS:
-			return {
-				...state,
-				users: action.payload,
-				loading: false,
-			};
-		case CLEAR_USERS:
-			return {
-				...state,
-				users: [],
-				loading: false,
-			};
-		case GET_REPOS:
-			return {
-				...state,
-				repos: action.payload,
-				loading: false,
-			};
-		case GET_USER:
-			return {
-				...state,
-				user: action.payload,
-				loading: false,
-			};
+  console.log(`Dispatched: ${action.type} Payload:`, action.payload);
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false,
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      };
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
 
-		case SET_LOADING:
-			return {
-				...state,
-				loading: true,
-			};
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
 
-		default:
-			break;
-	}
+    default:
+      break;
+  }
 };
