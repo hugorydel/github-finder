3ffbd0aec818a253e5b30128e787d6e6cf0d5923
Hugo Rydel <hugosteczkowski@gmail.com>
2020-03-30 22:11:03 -0400

    Deployment Prep

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,27 +3,27 @@
 import React from 'react';
 
 const About = () => {
-	return (
-		<div className='container bg-dark'>
-			<h1>About This App</h1>
-			<p>App to search Github users</p>
-			<p>Version 1.0.0</p>
-			<p>Create by Hugo Rydel</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sed
-				reiciendis nostrum est odio suscipit consequuntur. Facilis expedita
-				repellat nemo atque sit rerum cupiditate, omnis doloribus quod
-				exercitationem illum molestias odio hic quam quae earum quaerat dolore
-				facere dicta delectus praesentium blanditiis ipsa similique? Corrupti
-				autem est suscipit reprehenderit reiciendis, maiores odio sunt optio
-				sapiente in ipsum dolores beatae sint amet magnam. Ipsam expedita quasi
-				voluptates perspiciatis eaque itaque consequuntur vel enim eligendi,
-				aperiam rerum iusto eum harum qui ratione, similique delectus neque
-				cupiditate cumque quas obcaecati debitis laudantium optio. Sunt rem
-				veniam illo cupiditate perferendis? Expedita inventore iure quod.
-			</p>
-		</div>
-	);
+  return (
+    <div className='container bg-dark'>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <p>Version 1.0.0</p>
+      <p>Create by Hugo Rydel</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae sed
+        reiciendis nostrum est odio suscipit consequuntur. Facilis expedita
+        repellat nemo atque sit rerum cupiditate, omnis doloribus quod
+        exercitationem illum molestias odio hic quam quae earum quaerat dolore
+        facere dicta delectus praesentium blanditiis ipsa similique? Corrupti
+        autem est suscipit reprehenderit reiciendis, maiores odio sunt optio
+        sapiente in ipsum dolores beatae sint amet magnam. Ipsam expedita quasi
+        voluptates perspiciatis eaque itaque consequuntur vel enim eligendi,
+        aperiam rerum iusto eum harum qui ratione, similique delectus neque
+        cupiditate cumque quas obcaecati debitis laudantium optio. Sunt rem
+        veniam illo cupiditate perferendis? Expedita inventore iure quod.
+      </p>
+    </div>
+  );
 };
 
 export default About;
